fd223472aeb9da231583da970a041f5fee79cd11
phh2306 <phhoang.it.dev@gmail.com>
2023-10-28 08:36:35 +0700

    Back-end Server
    Set up init Back-end and run Hello World Back-end

---
+++ Backend/src/config/viewEngine.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+var express = require('express')
+
+let configViewEngine = () =>{
+    app.use(express.static("./src/public"));
+    app.set("view engine", 'ejs');
+    app.set("views", './src/views')
+}
+
+module.exports = configViewEngine
---
+++ Backend/src/server.js
@@ -0,0 +1,19 @@
+import express from  "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from './route/web';
+require('dotenv').config();
+
+let app = express();
+//Config app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+viewEngine(app);
+initWebRoutes(app);
+
+// Listen at port in env 
+let port = process.env.PORT || 2306;
+app.listen(port, () => {
+    console.log("Backend is running on port " + port)
+})
