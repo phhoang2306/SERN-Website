ce9c8e0c03bdcd4f349fbcc6de9f206905963e98
phh2306 <phhoang.it.dev@gmail.com>
2023-10-30 16:46:07 +0700

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -10,7 +10,7 @@ let app = express()
 //Config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-//app.use(cors({origin: true}));
+app.use(cors({origin: true}));
 
 viewEngine(app);
 initWebRoutes(app);
