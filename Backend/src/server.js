7d1bb9e672a8d73766229f13f6a4ec47072acf8c
phh2306 <phhoang.it.dev@gmail.com>
2023-10-30 16:37:20 +0700

    Fix API accesable
    Fix cors to access API from multi link

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,9 +4,10 @@ import viewEngine from "./config/viewEngine";
 import initWebRoutes from './route/web';
 import connectDB from "./config/connectDB";
 require('dotenv').config();
+import cors from 'cors';
 
 let app = express()
-
+app.use(cors({origin: true}));
 //Config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
