3ff37cfee0d8249fc76b1ffda41830da46401ab2
phh2306 <phhoang.it.dev@gmail.com>
2023-11-01 14:09:18 +0700

    Edit env

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 //app.use(cors({ credentials: true, origin: true }))
 app.use(function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000') // Allow access from link
+    res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT) // Allow access from link
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // Method allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
