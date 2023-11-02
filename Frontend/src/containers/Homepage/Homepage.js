11cb5bbc2872b692f3ea879d39688d31524c722f
phh2306 <phhoang.it.dev@gmail.com>
2023-11-02 16:59:19 +0700

    Set layout of FE

--- Frontend/src/containers/Homepage/Homepage.js
+++ Frontend/src/containers/Homepage/Homepage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Header from './Header';
+import Specialty from './Section/Specialty';
 class Homepage extends Component {
 
     render() {
         return (
             <div>
                 <Header/>
+                <Specialty/>
             </div>
         );
     }
---
+++ Frontend/src/containers/Homepage/Section/Specialty.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Specialty.scss'
+import image from '../../../assets/images/specialty/coxuongkhop.jpg'
+class Specialty extends Component {
+
+    handlechangeLanguage = (language) =>{
+        this.props.changeLanguageApp(language)
+    }
+    render() {
+        let settings = {
+            dots: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 4,
+            slidesToScrol: 2
+        };
+        return (
+            <div className='section-specialty'>
+                <div className='specialty-container'>
+                    <div className = 'header'>
+                        <span className = "tittle">Chuyên khoa phổ biến</span>
+                        <button className= 'btn-select'>Xem thêm</button>
+                    </div>
+                    <div className ='body'>
+                        <Slider {...settings}>
+                            <div className='slider-content'>
+                                <div className = 'image-content'></div>
+                                <span>Cơ xương khớp</span>
+                            </div>
+                            <div className='image-custome'>
+                                <h1>2</h1>
+                            </div>
+                            <div className='image-custome'>
+                                <h1>3</h1>
+                            </div>
+                            <div className='image-custome'>
+                                <h1>4</h1>
+                            </div> 
+                            <div className='image-custome'>
+                                <h1>5</h1>
+                            </div>
+                            <div className='image-custome'>
+                                <h1>6</h1>
+                            </div>
+                            <div className='image-custome'>
+                                <h1>7</h1>
+                            </div>
+                            <div className='image-custome'>
+                                <h1>8</h1>
+                            </div>  
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
